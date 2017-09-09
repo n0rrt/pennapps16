1b75188e1c41387a34ba3c5b6565aba760ebe390
n0rrt <timothy.harris@sjprephawks.org>
2017-09-09 13:10:35 -0700

    Revert "Revert "Update Texture.java""
    This reverts commit 334d4146e3550d05f87dd0582190498e8a35361a.

--- Texture.java
+++ Texture.java
@@ -2,30 +2,32 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-public class Texture{
-  public int[] pixels;
-  private String loc;
-  public final int SIZE;
-  public Texture(String location, int size){
-    loc = location;
-    SIZE = size;
-    pixels = new int[SIZE * SIZE];
-    load();
 
+public class Texture {
+ public int[] pixels;
+ private String loc;
+ public final int SIZE;
+ 
+ public Texture(String location, int size) {
+  loc = location;
+  SIZE = size;
+  pixels = new int[SIZE * SIZE];
+  load();
+ }
+ 
+ private void load() {
+  try {
+   BufferedImage image = ImageIO.read(new File(loc));
+   int w = image.getWidth();
+   int h = image.getHeight();
+   image.getRGB(0, 0, w, h, pixels, 0, w);
+  } catch (IOException e) {
+   e.printStackTrace();
   }
-  public void load(){
-    try{
-      BufferedImage image = ImageIO.read(new File(loc));
-      int w = image.getWidth();
-      int h = image.getHeight();
-      image.getRGB(0, 0, w, h, pixels, 0, w);
-    }
-    catch (IOException e) {
-        e.printStackTrace();
-    }
-  }
-  public static Texture metal = new Texture("res/wood.jpg", 64);
-  public static Texture window = new Texture("res/brick.jpg", 64);
-  public static Texture vent = new Texture("res/bluestone.jpg", 64);
-  public static Texture console = new Texture("res/stonebrick.jpg", 64);
+ }
+ 
+ public static Texture wood = new Texture("res/vent.png", 64);
+ public static Texture brick = new Texture("res/metal.png", 64);
+ public static Texture bluestone = new Texture("res/window.png", 64);
+ public static Texture stone = new Texture("res/console.png", 64);
 }

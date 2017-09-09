ad5e88d420a2a8d8c75482916d8e32ac258d55ae
n0rrt <timothy.harris@sjprephawks.org>
2017-09-09 05:13:17 -0700

    Added texture class

---
+++ Texture.java
@@ -0,0 +1,27 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+public class Texture{
+  public static Texture 
+  public int[] pixels;
+  private String loc;
+  public final int SIZE;
+  public Texture(String location, int size){
+    loc = location;
+    SIZE = size;
+    pixels = new int[SIZE * SIZE];
+    load();
+  }
+  public void load(){
+    try{
+      BufferedImage image = ImageIO.read(new File(loc));
+      int w = image.getWidth();
+      int h = image.getHeight();
+      image.getRGB(0, 0, w, h, pixels, 0, w);
+    }
+    catch (IOException e) {
+        e.printStackTrace();
+    }
+  }
+}
